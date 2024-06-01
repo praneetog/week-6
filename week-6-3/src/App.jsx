f5e2ac8558fdb8468c25ed0360677560f1881d6e
praneet <praneetkashyap001@gmail.com>
2024-06-01 22:56:08 +0530

    new commit

--- week-6-3/src/App.jsx
+++ week-6-3/src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [exchange1Data, setExchange1Data] = useState({});
+  const [exchange2Data, setExchange2Data] = useState({});
+  const [bankData, setBankData] = useState({});
+
+  useEffect(() => {
+    // Some operation to get the data
+    setExchange1Data({
+      returns: 100
+    });
+  }, [])
+
+  useEffect(() => {
+    // Some operation to get the data
+    setExchange2Data({
+      returns: 100
+    });
+  }, [])
+
+  useEffect(() => {
+    // Some operation to get the data
+    setTimeout(() => {
+      setBankData({
+        income: 100
+      });
+    })
+  }, [])
+
+  const cryptoReturns = exchange1Data.returns + exchange2Data.returns;
+  
+  const incomeTax = (cryptoReturns + bankData.income) * 0.3
 
   return (
-    <>
-      <h1>Hii</h1>
-    </>
+    <div>
+        hi there, your income tax returns are {incomeTax}
+    </div>
   )
 }
 
